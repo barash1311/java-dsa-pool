936b1f896642601e01c6f1e16f0c1e0a3025f449
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-08-15 13:26:42 +0530

    added reversal

--- src/com/Practice/LinkedList.java
+++ src/com/Practice/LinkedList.java
@@ -180,4 +180,16 @@ public class LinkedList {
         }
         return;
     }
+    public void reverse(){
+        Node prev=null;
+        Node current=head;
+        Node next=null;
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        head=prev;
+    }
 }

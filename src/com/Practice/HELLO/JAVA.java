206dcf23a71ba2a3c8366461a893f836eb9ecffd
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-09-02 18:17:54 +0530

    z program

--- src/com/Practice/HELLO/JAVA.java
+++ src/com/Practice/HELLO/JAVA.java
@@ -1,4 +1,13 @@
 package com.Practice.HELLO;
 
 public class JAVA {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *  ");
+        System.out.println("*****  ");
+    }
 }
+

92fa69f5659d4bd0259e16a87885549a375ba838
barashylogx1311 <138241217+barash1311@users.noreply.github.com>
2025-06-01 23:44:05 +0530

    gcd

--- src/com/RevisionMaths/GCD.java
+++ src/com/RevisionMaths/GCD.java
@@ -5,7 +5,9 @@ public class GCD {
         int number1=12;
         int number2=18;
         int result=gcd(number1,number2);
+        int result2=lcm2(number1,number2);
         System.out.println(result);
+        System.out.println(result2);
     }
 //    public static int gcd(int a,int b){
 //        int result=Math.min(a,b);
@@ -27,6 +29,18 @@ public class GCD {
     public static int gcd(int a,int b){
         if(b==0) return a;
         return gcd(b,a%b);
+    }
+    public static int lcm(int a,int b){
+        int  result=Math.min(a,b);
+        while(true){
+            if(result%a==0 && result%b==0){
+                return result;
+            }
+            result++;
+        }
+    }
+    public static int lcm2(int a,int b){
+        return (a*b)/gcd(a,b);
     }
 }
 

b2918c205b41668dd0e9ff4031047f97bb5f0a28
barashylogx1311 <138241217+barash1311@users.noreply.github.com>
2025-06-02 23:57:42 +0530

    bitwise

--- src/com/RevisionMaths/Count.java
+++ src/com/RevisionMaths/Count.java
@@ -1,4 +1,33 @@
 package com.RevisionMaths;
 
 public class Count {
+    public static void main(String[] args) {
+        int output=countSetBits(16);
+        System.out.println(output);
+    }
+    public static int countSetBits(int n){
+        String binary=Integer.toBinaryString(n);
+        int count=0;
+        for(int i=0;i<binary.length();i++){
+            if(binary.charAt(i)=='1')count++;
+        }
+        return count;
+    }
+    public static int countSetBits2(int n){
+        int count=0;
+        while(n>0){
+            count+=n%2;
+            n/=2;
+        }
+        return count;
+    }
+    public static int countSetBits3(int n){
+        int count=0;
+        while(n>0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+
 }
